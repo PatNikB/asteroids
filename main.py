ef02c753d30ea9110fe6449bb83291dd31cdf77f
PatNikB <patelnikeshb@gmail.com>
2024-08-28 00:10:13 +0100

    add container class for Player.update and Player.draw methods

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def main():
     oClock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
+
     oPlayer = Player( ( SCREEN_WIDTH / 2 ), ( SCREEN_HEIGHT / 2 ))
 
     while True:
@@ -21,8 +25,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        oPlayer.update(dt)
-        oPlayer.draw(screen)
+
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
+
 
         pygame.display.flip()
         dt = oClock.tick(60) / 1000
